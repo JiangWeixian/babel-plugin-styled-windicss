e55f850935753d77cdd4e0650441a27dc9140557
jiangWeixian <jiangweixian1994@gmail.com>
2021-09-11 14:55:35 +0800

    :bug:  regex

--- src/visitors/templateLiterals/transpile.js
+++ src/visitors/templateLiterals/transpile.js
@@ -2,6 +2,8 @@ import { isStyled, isHelper } from '../../utils/detectors'
 import { parse } from '../../utils/windicss'
 import { log } from '../log'
 
+const APPLY_REGEX = /(.*)@apply([^`\${};]*);\n/gm
+
 export default (t) => (path, state) => {
   if (isStyled(t)(path.node.tag, state) || isHelper(t)(path.node.tag, state)) {
     const {
@@ -16,7 +18,7 @@ export default (t) => (path, state) => {
           log.template.input(quasi.value.cooked)
           if (/@apply/g.test(quasi.value.cooked)) {
             const next = quasi.value.cooked.replace(
-              /(.*)@apply([^`${}]*)\n/gm,
+              APPLY_REGEX,
               (_match, pre, applyCss) => {
                 const parsed = parse(`&{@apply ${applyCss}}`)
                 return `${pre} ${parsed}`
